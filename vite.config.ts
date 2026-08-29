85e88dfc7dec7172d1beecbd8c9751d5afbee9e6
Shubham <im.shubhamlal@gmail.com>
2026-08-29 11:09:27 +0530

    rm vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import adapterVercel from '@sveltejs/adapter-vercel';
-import adapterNode from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	const PORT = parseInt(env.PORT || '5000', 10);
-
-	const isVercel = process.env.VERCEL === '1';
-	const adapter = isVercel ? adapterVercel() : adapterNode({ precompress: true });
 
 	return {
 		server: {
@@ -21,7 +17,7 @@ export default defineConfig(({ mode }) => {
 				compilerOptions: {
 					runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 				},
-				adapter
+				adapter: adapter({ precompress: true })
 			}),
 			tailwindcss(),
 		]
