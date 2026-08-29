2d4abe8432ddcdcd23034bed5d75ff3d8a3b8d28
Shubham <im.shubhamlal@gmail.com>
2026-08-29 10:34:55 +0530

    test vercel deploymemt

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-	plugins: [
-		sveltekit({
-			compilerOptions: {
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter({ precompress: true })
-		}),
-		tailwindcss(),
-	]
+export default defineConfig(() => {
+	const isVercel = process.env.VERCEL === '1';
+	const adapter = isVercel ? adapterVercel() : adapterNode({ precompress: true });
+
+	return {
+		plugins: [
+			sveltekit({
+				compilerOptions: {
+					runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+				},
+				adapter
+			}),
+			tailwindcss(),
+		]
+	};
 });
