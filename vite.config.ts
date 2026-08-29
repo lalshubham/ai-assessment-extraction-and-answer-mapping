82db55c8c65092eaa50b0aab618b10586662c022
Shubham <im.shubhamlal@gmail.com>
2026-08-29 10:54:18 +0530

    deploy to cloud

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterNode from '@sveltejs/adapter-node';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const PORT = parseInt(env.PORT || '5000', 10);
+
 	const isVercel = process.env.VERCEL === '1';
 	const adapter = isVercel ? adapterVercel() : adapterNode({ precompress: true });
 
 	return {
+		server: {
+			port: PORT,
+			strictPort: true
+		},
 		plugins: [
 			sveltekit({
 				compilerOptions: {
